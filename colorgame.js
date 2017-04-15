89624c1d127edb7bb59f5f23225325d166b54803
Samuel Adusei-Arthur <samuel-adusei@hotmail.com>
2017-04-15 16:36:14 +0100

    Update colorgame.js

--- colorgame.js
+++ colorgame.js
@@ -1,33 +1,47 @@
-var colors =	[
+var colors = [
 	"rgb(255, 0, 0)",
 	"rgb(255, 255, 0)",
 	"rgb(0, 255, 0)",
 	"rgb(0, 255, 255)",
 	"rgb(0, 0, 255)",
 	"rgb(255, 0, 255)"
-	]
-	
+];
+
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
-//first where hard coding the ting 
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
-for(var i = 0; i < squares.length; i++) {
-		//add initial colors to square
-		squares[i].style.background = colors[i];
-		
+for(var i = 0; i < squares.length; i++){
+	// add initial colors to squares
+	squares[i].style.background = colors[i];
+
 	//add click listeners to squares
-	squares[i].addEventListener("click", function()	{
-		//grab color of clicked square
-	var clickedColor = this.style.background;
-		//compare color to pickColor
+	squares[i].addEventListener("click", function() {
+		//grab color of clicked squares
+		var clickedColor = this.style.background;
+		//compare color to pickedColor
 		if(clickedColor === pickedColor) {
-			alert("Correct!");
-		}else {
-			alert("Wrong");
+			messageDisplay.textContent = "Correct!";
+			changeColors(clickedColor);
+		} else {
+			this.style.background = "#232323";
+			messageDisplay.textContent = "Try Again";
 		}
-		});
-		}
-// this colours the squares in order 
+	});
+}
+
+function changeColors(color) {
+	//loop through all squares
+	for(var i = 0; i < squares.length; i++) {
+		//change each color to match given color
+		squares[i].style.background = color;
+	}
+}
+
+function pickColor() {
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
